b1abce3c68cd3a911b5d35398b2e507ea9e90eeb
maxgbesong <mgbesong@gmail.com>
2026-06-16 17:10:37 +0000

    Initial setup

---
+++ FinalProject/FinalProject/ConsoleUI.cs
@@ -0,0 +1,18 @@
+namespace FinalProject;
+
+using Spectre.Console;
+
+public class ConsoleUI
+{
+    
+
+    public ConsoleUI()
+    {
+        
+    }
+
+    public void Show()
+    {
+        
+    }
+}
---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConsoleUI ui = new ConsoleUI();
+        ui.Show();
+    }
+}
